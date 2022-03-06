adcc5b632afd16b8a2450ea95414010428be187f
Sergey <antonov2703@mail.ru>
2022-03-06 23:03:33 +0300

    work

---
+++ src/test/java/school/lesson1/HomeWorkApp.java
@@ -0,0 +1,9 @@
+package school.lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
